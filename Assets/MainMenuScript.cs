0ee7cb00a4b9bba138bc05057894c8e903a854ab
Saswat Mishra <saswatmishra15@gmail.com>
2018-01-27 19:48:47 +0530

    UI added

---
+++ Assets/MainMenuScript.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuScript : MonoBehaviour {
+    public GameObject LevelObj;
+
+    public void Close(){
+        Application.Quit();
+    }
+
+    public void Play(){
+       int currentLevel = PlayerPrefs.GetInt("currentLevel", 0);
+       SceneManager.LoadScene("Level_"+currentLevel);
+    }
+
+    public void Levels(){
+        LevelObj.SetActive(true);    
+    }
+
+    public void BackToMenu(){
+        LevelObj.SetActive(false);
+    }
+
+    public void OpenLevel(int index){
+        SceneManager.LoadScene("Level_" + index); 
+    }
+}
---
+++ Assets/UIController.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class UIController : MonoBehaviour {
+    public Text resultText, resultBtnText;
+    public Button resultBtn;
+    public GameObject GameOverPanel;
+
+    public int totalLevels;
+    int currentLevel;
+
+    void Start(){
+        currentLevel = PlayerPrefs.GetInt("currentLevel", 0);
+    }
+
+    public void GameOver(){
+        GameOverPanel.SetActive(true);
+        if(true){
+            resultText.text = "LEVEL COMPLETE";
+            resultBtnText.text = "NEXT";
+            if (currentLevel == totalLevels)
+                resultBtn.interactable = false;
+        }else{
+            resultText.text = "LEVEL FAILED";
+            resultBtnText.text = "RETRY";
+        }
+
+        //if game complete, change resultText to level complete and resultBtnText to Retry
+         //if game failed, change resultText to level failed and resultBtnText to NEXT
+        //if last level and level is complete, disabled the resultBtn
+    }
+
+    public void Result(){
+        if(true){
+            currentLevel++;
+            PlayerPrefs.SetInt("currentLevel", currentLevel);
+            SceneManager.LoadScene("Level_" + currentLevel);
+        }else{
+            SceneManager.LoadScene("Level_" + currentLevel);
+        }
+    }
+
+    public void GotoMenu(){
+        SceneManager.LoadScene("MainMenu");
+    }
+}
